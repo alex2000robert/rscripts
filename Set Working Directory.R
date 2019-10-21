4aa7b31ebf4f253900da70d8ea88d888ee3a336b
alex2000robert <alex2000robert@yahoo.com>
2019-10-22 00:04:13 +0100

    Update Set Working Directory.R

--- Set Working Directory.R
+++ Set Working Directory.R
@@ -1,5 +1,20 @@
 # Script for finding the working directory of the current project, by using the name of the R script file. 
 # Very useful if you constantly need to move a project between computers.
+#    Copyright (C) 2019 Alexandru Robert Lazar
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 # REQUIRED: Set your script's file name in the line "filename". The rest will be done automatically.
 filename = "filename.R"
